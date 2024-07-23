54809e2bf37a3f4b614906a98e99a6e311ffa362
Lu-nas <luanasilva.ss9497@gmail.com>
2024-07-23 00:19:26 -0300

    CRUD-do-Produto

--- src/main/java/com/generation/farmacia/model/Produto.java
+++ src/main/java/com/generation/farmacia/model/Produto.java
@@ -1,5 +1,80 @@
 package com.generation.farmacia.model;
 
+import java.math.BigDecimal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+@Entity
+@Table(name = "Produtos")
 public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	 @NotNull(message = "O  nome é obrigatório")
+	 @Size(min = 3, max = 50, message = "O nome deve ter entre 3 e 100 caracteres")
+     private String nome;
+	 
+	 @NotNull(message = "A descrição é obrigatória")
+	 @Size(max = 360, message = "A descrição deve ter no máximo 360 caracteres")
+     private String descricao;
+	 
+	 @NotNull(message = "Inserir o  preço ")
+	 private BigDecimal preco;
+
+	 @ManyToOne
+     @JoinColumn(name = "categoria_id")
+     private Categoria categoria;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
+	}
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+
+	 
 
 }
