43bc096e9b4505a6c6745661404c86ea9af28ea9
Lu-nas <Luanasilva.ss9497@gmail.com>
2026-01-20 19:32:11 -0300

    task:  Cria entidade Produto

---
+++ src/main/java/com/farmacia/crudfarmacia/model/produto.java
@@ -0,0 +1,68 @@
+package com.farmacia.crudfarmacia.model;
+
+import java.math.BigDecimal;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "produtos")
+public class produto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String descricao;
+
+    private BigDecimal preco;
+
+    private Integer quantidade;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public BigDecimal getPreco() {
+        return preco;
+    }
+
+    public void setPreco(BigDecimal preco) {
+        this.preco = preco;
+    }
+
+    public Integer getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(Integer quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    // getters e setters
+
+    
+    
+}
