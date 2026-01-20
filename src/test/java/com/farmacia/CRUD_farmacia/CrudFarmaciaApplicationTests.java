d7567584cad6b80a028443f8dc02825b5d6e7949
Lu-nas <Luanasilva.ss9497@gmail.com>
2026-01-20 19:11:25 -0300

    tarefa: Criar projeto Spring Boot inicial

---
+++ src/test/java/com/farmacia/CRUD_farmacia/CrudFarmaciaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.farmacia.CRUD_farmacia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudFarmaciaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
